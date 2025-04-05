708b950d788be95e56688245d8ce205974ac7102
yeedaKing <143111017+yeedaKing@users.noreply.github.com>
2025-04-05 03:49:24 +0000

    CMakeLists update

---
+++ build/CMakeFiles/sta_lta_detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sta_lta_detector.dir/main.cpp.o"
+  "sta_lta_detector"
+  "sta_lta_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sta_lta_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
